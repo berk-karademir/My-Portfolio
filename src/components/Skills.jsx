99653d7d0c57e0888047d10988686970df0c870c
Berk KARADEMİR <berk.karademir@hotmail.com>
2025-03-31 22:56:49 +0300

    skills updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import { languageData } from "../../myData.js";
 import { LanguageContext } from "../context/LanguageContext";
@@ -7,6 +7,9 @@ function Skills() {
   const { currentLanguage } = useContext(LanguageContext);
   const { currentTheme } = useTheme();
   const { skillsHeading, currentSkills } = languageData[currentLanguage].skills;
+  const [expanded, setExpanded] = useState(false);
+
+  const visibleSkills = expanded ? currentSkills : currentSkills.slice(0, 8);
 
   return (
     <section
@@ -18,18 +21,28 @@ function Skills() {
     >
       <h2 className="py-16 text-4xl">{skillsHeading}</h2>
       <div className="flex flex-row justify-center">
-        <div className="flex gap-4 text-center pb-24">
-          {currentSkills.map((skill, index) => (
+        <div className="flex justify-center gap-6 text-center pb-6 max-w-[700px] flex-wrap">
+          {visibleSkills.map((skill, index) => (
             <div key={index} className="font-bold text-l">
               <img
                 src={`images/${skill.toLowerCase()}-logo.png`}
                 alt={`${skill} Logo`}
+                width={"128px"}
+                className="rounded-2xl"
               />
               <span>{skill}</span>
             </div>
           ))}
         </div>
       </div>
+      {currentSkills.length > 8 && (
+        <button
+          className="px-4 py-2 mb-6 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? "Show Less ▲" : "Expand All ▼"}
+        </button>
+      )}
     </section>
   );
 }
